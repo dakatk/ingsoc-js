7105c79cf93dacf8c580c52b2d5e100faf388d0f
dakatk <dakatk97@gmail.com>
2022-10-30 10:30:40 -0500

    Fixed listener binding

--- example.js
+++ example.js
@@ -12,15 +12,16 @@ BigBrother.registerPartyMember({
         showTitle: true,
     },
     listeners: {
-        switchClass: function() {
+        switchClass() {
+            console.log(this);
             const titleClass = this.watchers.titleClass;
             this.watchers.titleClass = (titleClass === 'red' ? 'blue' : 'red');
         },
-        switchId: function() {
+        switchId() {
             const titleId = this.watchers.titleId;
             this.watchers.titleId = (titleId === 'bg-cyan' ? 'bg-yellow' : 'bg-cyan');
         },
-        toggleTitle: function(event) {
+        toggleTitle(event) {
             const showTitle = event.target.checked;
             this.watchers.showTitle = showTitle;
         }
@@ -29,7 +30,14 @@ BigBrother.registerPartyMember({
 
 BigBrother.registerPartyMember({
     id: 'date-component',
-    watchers: { date: '2021-02-01' }
+    watchers: { date: '2021-02-01' },
+    listeners: {
+        fromDateObject(date) {
+            const day = (`0${date.getDate()}`).slice(-2);
+            const month = (`0${date.getMonth() + 1}`).slice(-2);
+            this.watchers.date = `${date.getFullYear()}-${month}-${day}`;
+        }
+    }
 });
 
 BigBrother.registerPartyMember({
@@ -38,10 +46,15 @@ BigBrother.registerPartyMember({
         text: ['Zero', 'One', 'Two']
     },
     listeners: {
-        updateText: function(event) {
+        updateText(event) {
             this.watchers.text[0] = event.target.value;
             this.watchers.text[2] = this.parent.watchers.message;
-            console.log(this.children);
+
+            const example = this.children.example;
+            const date = new Date(`${example.watchers.date}T00:00`);
+
+            date.setDate(date.getDate() + 1);
+            example.listeners.fromDateObject(date);
         }
     }
 });
